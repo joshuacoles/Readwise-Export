e5e77ba077de220ff1ddcc01da4dba5edb69930b
Joshua Coles <josh@coles.to>
2024-02-04 09:13:32 +0000

    Update export script to use new options

--- live-export.sh
+++ live-export.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env zsh
+set -euxo pipefail
 
+VAULT="${VAULT:-"/Users/joshuacoles/Obsidian Sync/My Life"}"
+
+args=(
+  --vault "$VAULT"
+  --base-folder "Readwise"
+  --api-token "$READWISE_TOKEN"
+  --library "$VAULT/Readwise/library.json"
+  --book-template "$PWD/templates/book.md.tera"
+  --highlight-template "$PWD/templates/highlight.md.tera"
+  --replacement-strategy "replace"
+)
+
+# Update the library
 ./target/release/obsidian-readwise-rs \
-  --vault "/Users/joshuacoles/Obsidian Sync/My Life" \
-  --base-folder "Readwise" \
-  --api-token "$READWISE_TOKEN" \
-  --library "/Users/joshuacoles/Obsidian Sync/My Life/Readwise/library.json" \
-  --book-template "$PWD/templates/book.md.tera" \
-  --highlight-template "$PWD/templates/highlight.md.tera" \
-  --replacement-strategy "replace" \
-  --refetch
+  "${args[@]}" \
+  --fetch update \
+  --no-export
+
+# Skip articles if there are no highlights
+./target/release/obsidian-readwise-rs \
+  "${args[@]}" \
+  --fetch cache \
+  --filter-category "articles" \
+  --skip-empty
+
+# For books we want to write a file even if there are no highlights
+./target/release/obsidian-readwise-rs \
+  "${args[@]}" \
+  --fetch cache \
+  --filter-category "books"
